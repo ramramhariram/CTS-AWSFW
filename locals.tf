510102a8a3e0150a001c0ab3991cc239bacfc11a
ramramhariram <hari@hashicorp.com>
2022-06-13 16:31:33 -0700

    updating issue tag

--- locals.tf
+++ locals.tf
@@ -3,8 +3,8 @@ locals {
   common_tags = {
     owner   = "hari"
     se-region = "EP"
-    purpose = "customer_demo_env, ttl is set to -1 since i am new and only as i am ramping up, i will tune it once i understand my needs better" 
-    ttl = "-1"
+    #purpose = "customer_demo_env, ttl is set to -1 since i am new and only as i am ramping up, i will tune it once i understand my needs better" 
+   # ttl = "-1"
     terraform = true
     hc-internet-facing = false    
   }
